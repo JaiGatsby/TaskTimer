87b2cd6a13548fb31ca104266b6bc3e351c78c6b
JaiGatsby <jais334@gmail.com>
2017-04-11 16:44:59 +0800

    Linked to a collection for persistent storage

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,23 @@
 import {Template} from 'meteor/templating';
 
+import { Tasks } from '../api/tasks.js';
+
 import './body.html';
 // body section can be accessed in js by Template.body
 Template.body.helpers({
-	tasks: [
-	{text: 'This is task 1'},
-	{text: 'This is task 2'},
-	{text: 'This is task 3'},
-	],
+	tasks(){
+		return Tasks.find({});
+	}
 });
+
+// Template.body.events({
+// 	'submit .new-task'(event) {
+// 		event.preventDefault(); // Prevent default(empty) form submit
+
+// 		// Get value
+// 		const target = event.target;
+// 		const text = target.text.value;
+
+// 		// Insert a task into t
+// 	}
+// });
